edb125e1560ba470b68321a8643315ae626c8dea
Ben Lavender <blavender@gmail.com>
2011-01-28 19:52:56 +0100

    Monkey patch the documentation formatter not to repeat context groups that span multiple files

---
+++ spec/support/extensions/documentation_formatter.rb
@@ -0,0 +1,34 @@
+module RSpec
+  module Core
+    module Formatters
+
+      ## allow contexts in the documentation formatter to span multiple files.
+      # This is pretty brittle, but was rejected upstream as being too special a
+      # case.
+      class DocumentationFormatter
+
+        alias_method :old_example_group_started, :example_group_started
+        def example_group_started(example_group)
+
+          if previous_groups.empty? || example_group.description != previous_groups.pop
+            previous_groups.clear
+            old_example_group_started(example_group)
+          else
+            @group_level += 1
+          end
+        end
+
+        alias_method :old_example_group_finished, :example_group_finished
+        def example_group_finished(example_group)
+          previous_groups.push example_group.description
+          old_example_group_finished(example_group)
+        end
+
+        def previous_groups
+          @previous_groups ||= []
+        end
+
+      end
+    end
+  end
+end
