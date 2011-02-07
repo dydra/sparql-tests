461498b82c4c39664fcf08226f9b4f1ab722e22b
Ben Lavender <blavender@gmail.com>
2011-02-08 00:46:17 +0100

    Added a CSV test formatter for use in profiling runs

---
+++ Rakefile
@@ -0,0 +1,22 @@
+
+require 'bundler'
+Bundler.setup
+Bundler.require(:default)
+require 'rspec/core/rake_task'
+
+namespace :spec do
+  RSpec::Core::RakeTask.new('csv') do |t|
+    file = File.dirname(__FILE__) + '/spec/support/formatters/csv_formatter.rb'
+    t.rspec_opts = []
+    t.rspec_opts  << ['--require', file]
+    t.rspec_opts << ['--format', 'CSVFormatter']
+    t.rspec_opts << ['--tag', '~values:lexical']
+    t.rspec_opts << ['--tag', '~arithmetic:boxed']
+    t.rspec_opts << ['--tag', '~blank_nodes:unique']
+    t.rspec_opts << ['--tag', '~reduced:all']
+    t.rspec_opts << ['--tag', '~status:bug']
+    t.pattern = 'spec/w3c/data-r2/**/*.rb'
+    t.verbose = false
+  end
+end
+
---
+++ spec/support/formatters/csv_formatter.rb
@@ -0,0 +1,53 @@
+require 'rspec/core/formatters/base_text_formatter'
+
+class CSVFormatter < RSpec::Core::Formatters::BaseTextFormatter
+
+  def initialize(output)
+    super(output)
+    @groups = []
+  end
+
+  def example_group_started(example_group)
+    @groups.push(example_group.description)
+  end
+
+  def example_group_finished(example_group)
+    @groups.pop
+  end
+
+  def example_passed(example)
+    super(example)
+    output.puts passed_output(example)
+  end
+
+  def example_pending(example)
+    super(example)
+    output.puts pending_output(example, example.execution_result[:pending_message])
+  end
+
+  def example_failed(example)
+    super(example)
+    output.puts failure_output(example, example.execution_result[:exception])
+  end
+
+  def failure_output(example, exception)
+    csv_output(example,'failed')
+  end
+
+  def passed_output(example)
+    csv_output(example,'passed')
+  end
+
+  def pending_output(example, message)
+    csv_output(example,'pending')
+  end
+
+  def csv_output(example, status)
+    "#{status}\t#{example.execution_result[:run_time]}\t#{@groups.last}/#{example.description}"
+  end
+
+  def example_group_chain
+    example_group.ancestors.reverse
+  end
+
+end
