8deba06d5ebf3a12d4f3fabd54c852eb56d0a5b5
james <james@datagraph.org>
2011-04-15 22:17:36 +0000

    tests for conjunction/disjunction compilation problems

---
+++ spec/datagraph/unfuddle/dydra-265/265-55dbe790_spec.rb
@@ -0,0 +1,56 @@
+# coding: utf-8
+#
+require 'spec_helper'
+
+# Test handling standard and non-standard escapes
+# 
+#
+
+describe "unfuddle ticket" do
+  context "265" do
+    before :all do
+     @url = 'http://dydra.com/mglcel/psw.nt'
+     @query = %q{
+SELECT * WHERE {
+ ?of1 ?pf1 <http://parisemantique.fr/entities/Way/rue_de_la_plaine> .
+ ?of2 ?pf2 ?of1 .
+ ?middle ?pf3 ?of2 .
+ ?middle ?ps1 <http://parisemantique.fr/entities/School/ecole_elementaire_40_rue_des_pyrenees> .
+ FILTER ((?pf1 != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ) &&
+         (?pf2 != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ) &&
+         (?pf3 != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ) &&
+         (?ps1 != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ) &&
+         (!isLiteral(?middle))&&
+         (?middle != <http://parisemantique.fr/entities/Way/rue_de_la_plaine> ) &&
+         (?middle != <http://parisemantique.fr/entities/Way/rue_de_la_plaine> ) &&
+         (?middle != <http://parisemantique.fr/entities/School/ecole_elementaire_40_rue_des_pyrenees> ) &&
+(?middle != ?of1 )  &&
+(?middle != ?of2 ) &&
+(!isLiteral(?of1)) &&
+(?of1 != <http://parisemantique.fr/entities/Way/rue_de_la_plaine> ) &&
+(?of1 != <http://parisemantique.fr/entities/School/ecole_elementaire_40_rue_des_pyrenees> ) &&
+(?of1 != ?middle ) &&
+(?of1 != ?of2 ) &&
+(!isLiteral(?of2)) &&
+(?of2 != <http://parisemantique.fr/entities/Way/rue_de_la_plaine> ) &&
+(?of2 != <http://parisemantique.fr/entities/School/ecole_elementaire_40_rue_des_pyrenees> ) &&
+(?of2 != ?middle ) && (?of2 != ?of1 ) ).
+  }
+  LIMIT 10
+}
+    end
+
+    example "mglcel" do
+    
+      graphs = {}
+      graphs[:default] = { :url => @url, :format => :nt}
+
+
+      repository = 'dydra-265'
+      expected_length = 8
+
+      sparql_query(:graphs => graphs, :query => @query,       # unordered comparison in rspec is =~
+                   :repository => repository, :form => :select).length.should == expected_length
+    end
+  end
+end
