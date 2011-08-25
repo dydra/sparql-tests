c06e9de4c5d8497fc6662765abd530f616d5d1b0
james anderson <james.anderson@setf.de>
2011-08-25 15:39:10 +0200

    replace rdf w/ ttl

--- spec/w3c/sparql11/subquery/subquery01-ttl_spec.rb
+++ spec/w3c/sparql11/subquery/subquery01-ttl_spec.rb
@@ -48,7 +48,7 @@ graph ?g {
       graphs = {}
       graphs[:default] = nil
 
-      graphs[RDF::URI('sq01.rdf')] = { :data => @graph0, :format => :rdf }
+      graphs[RDF::URI('sq01.rdf')] = { :data => @graph0, :format => :ttl }
 
       repository = 'subquery-subquery01'
       expected = [
