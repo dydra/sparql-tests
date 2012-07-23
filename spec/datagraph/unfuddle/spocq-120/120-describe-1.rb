beefdf58f1b868167f0cd66dca1e0618c42b122d
james anderson <james.anderson@setf.de>
2012-07-24 00:38:28 +0200

    spocq#120: test for describe on a solution variable

---
+++ spec/datagraph/unfuddle/spocq-120/120-describe-1.rb
@@ -0,0 +1,47 @@
+# coding: utf-8
+#
+require 'spec_helper'
+
+describe "unfuddle" do
+  context "spocq 120" do
+    before :all do
+      @data = %q{
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+
+<http://www.example.org/instance#a>
+    <http://www.example.org/schema#p> 1 ;
+    <http://www.example.org/schema#q> 2 .
+
+<http://www.example.org/instance#b>
+    <http://www.example.org/schema#p> 3 .
+}
+      @query = %q{
+describe ?s
+where { ?s <http://www.example.org/schema#p> ?o }
+}
+    end
+
+    example "describe subject" do
+    
+      graphs = {}
+      graphs[:default] = { :data => @data, :format => :ttl}
+
+
+      repository = '102-describe'
+      expected = RDF::Graph.new do |graph|
+        graph << RDF::Statement.new(
+          :subject => RDF::URI('http://www.example.org/instance#a'),
+          :predicate => RDF::URI('http://www.example.org/schema#p'),
+          :object => RDF::Literal.new(1))
+        graph << RDF::Statement.new(
+          :subject => RDF::URI('http://www.example.org/instance#a'),
+          :predicate => RDF::URI('http://www.example.org/schema#q'),
+          :object => RDF::Literal.new(2))
+
+      end
+
+      sparql_query(:graphs => graphs, :query => @query,
+                   :repository => repository, :form => :describe).should be_isomorphic_with expected
+    end
+  end
+end
